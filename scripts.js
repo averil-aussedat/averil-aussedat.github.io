fa5c013a99defbf1cf4e5d8e7e95b22521084965
Averil Prost <averilprost33@gmail.com>
2026-07-09 21:08:12 +0200

    "renew"

--- scripts.js
+++ scripts.js
@@ -1,9 +1,31 @@
-// Renders an element visible. Beware, the argument must be the unique ID.
+
+// Renders an element visible
 function toggleVisibility(theID) {
   var elem = document.getElementById(theID);
-  if ((elem.style.display === "") || (elem.style.display === "none")) {
-    elem.style.display = "block";
-  } else {
+  var caret = document.getElementById(theID + 'caret');
+  if (elem.style.display === "block") {
     elem.style.display = "none";
+    caret.style.rotate = "0deg";
+  } else {
+    elem.style.display = "block";
+    caret.style.rotate = "90deg";
+  }
+}
+
+// renders all sections visible
+function toggleSections() {
+  var sects = document.getElementsByClassName("sectioncontent");
+  firstsect = sects[0];
+
+  if (firstsect.style.display === "block") {
+    for (const thesect of sects) {
+      thesect.style.display = "none";
+      document.getElementById(thesect.id + 'caret').style.rotate = "0deg";
+    }
+  } else {
+    for (const thesect of sects) {
+      thesect.style.display = "block";
+      document.getElementById(thesect.id + 'caret').style.rotate = "90deg";
+    }
   }
 }
